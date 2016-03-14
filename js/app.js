ce209bbbaa00aa1dd4d40c284449833271731bad
Romain Quilliot <romain.addweb@gmail.com>
2016-03-14 02:18:14 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(function () {
+    // Anim startup
+    setTimeout(function () {
+        $('.main-title').addClass('active');
+        $('.main-subtitle').addClass('active');
+        $('.button-container').addClass('active');
+    }, 200);
+
+
+    // Open side content
+    $('#knowme').on('click', function () {
+        $('#about-overlay').addClass('active');
+
+        setTimeout(function () { 
+            $('#about-content').addClass('active');
+            $('#home').addClass('active');
+
+            // Display item effect
+            var items = $('.work .item');
+            var i = 1;
+            items.each(function () {
+                setTimeout(function() { $(this).addClass('active'); }.bind(this), (i * 300));
+                i++;
+            });
+        }, 200);
+    });
+
+    // Close side content
+    $('#about-overlay').on('click', function () {
+        $('#about-overlay').removeClass('active');
+        $('#about-content').removeClass('active');
+        $('#home').removeClass('active');
+        $('.work .item').removeClass('active');
+    });
+});
